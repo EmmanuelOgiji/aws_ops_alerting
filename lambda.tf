40188a3214b367fbe729c01c17829695a12da73a
EmmanuelOgiji <epiusogiji@yahoo.com>
2021-09-01 10:01:54 +0100

    - Add SNS permission for lambda

--- lambda.tf
+++ lambda.tf
@@ -51,3 +51,11 @@ resource "aws_lambda_function" "alerting_lambda" {
     }
   }
 }
+
+resource "aws_lambda_permission" "with_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.alerting_lambda.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.ops_topic.arn
+}
--- sns.tf
+++ sns.tf
@@ -1,3 +1,9 @@
 resource "aws_sns_topic" "ops_topic" {
   name = "alerts-topic"
 }
+
+resource "aws_sns_topic_subscription" "lambda" {
+  topic_arn = aws_sns_topic.ops_topic.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.alerting_lambda.arn
+}
